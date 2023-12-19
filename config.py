bb59ab6979e355236284596fb79ef9f2d4d5b239
EHii2001 <118877821+EHii2001@users.noreply.github.com>
2023-12-19 11:44:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cmkhb6xfujxd.us-east-1.rds.amazonaws.com"
+customuser = "aws_users"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "enochhcs-employee"
+customregion = "us-east-1"
+
